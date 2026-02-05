ede64dccf78b5f5d0fae3ace5b9b83021d8d511b
Walaa Elsawi <walaatealab@gmail.com>
2026-02-05 02:44:48 +0200

    Home screen ui , utils

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color whiteColor = Color(0xffF2FEFF);
+  static const Color darkWhitColor = Color(0xffcee2ea);
+  static const Color darkBlueColor = Color(0xff10254e);
+  static const Color blueColor = Color(0xff94c0cd);
+  static const Color lavenderColor = Color(0xffbca9dd);
+  static const Color mintColor = Color(0xffa6e6dc);
+  static const Color lightGrayColor = Color(0x7e95969a);
+  static const Color mediumGrayColor = Color(0xff515151);
+  static const Color darkGrayColor = Color(0xff333333);
+
+}
